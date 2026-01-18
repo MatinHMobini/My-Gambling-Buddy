e5f89c88c1e8b0a26394f46eb35c462b9ad4e533
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2026-01-17 20:35:37 -0500

    Update next.config.ts

--- gambling-buddy/next.config.ts
+++ gambling-buddy/next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
